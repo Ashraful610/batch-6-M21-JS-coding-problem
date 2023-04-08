c69e75aec1f9a5512343484642bbbd396fefaa17
Ashraful610 <ashalam610@gmail.com>
2023-04-08 21:46:28 +0600

    string method indexOf , inClues , startsWith , endsWith

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var name = 'Ashraful Alam';
 var capital = name.toUpperCase();
-console.log(capital)
+var name2 = 'Mahmudulla'
+var lowercase = name.toLowerCase();
+
+console.log(capital , lowercase)
+console.log(name.includes('ful'))
+console.log(name.startsWith('A'))
+console.log(name.endsWith('s'));
