bfc633edb0c8211d44418b252ad66cc3e1afc345
Ashraful610 <ashalam610@gmail.com>
2023-04-09 09:56:23 +0600

    math operation

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ var capital = name.toUpperCase();
 var name2 = 'Mahmudulla'
 var lowercase = name.toLowerCase();
 
-console.log(capital , lowercase)
-console.log(name.includes('ful'))
-console.log(name.startsWith('A'))
-console.log(name.endsWith('s'));
+// console.log(capital , lowercase)
+// console.log(name.includes('ful'))
+// console.log(name.startsWith('A'))
+// console.log(name.endsWith('s'));
+
+var number = Math.pow(4 , 4)
+var num1 = 23;
+var num2 = 42;
+var result = Math.abs(num1 - num2);
+var result2 = Math.ceil(3.23)
+var result3 = Math.floor(3.23)
+var result4 = Math.random() 
+
+var result5 = Math.round(Math.random() * 20)
+
+console.log(result5);
